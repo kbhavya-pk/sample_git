941a74294bbba840dd08145b1699a2999cf1407e
kbhavya-pk <kbhavya@prokarma.com>
2018-03-19 18:37:47 +0530

    First

---
+++ src/main/java/com/proj/prok/sample_git/App.java
@@ -0,0 +1,13 @@
+package com.proj.prok.sample_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
